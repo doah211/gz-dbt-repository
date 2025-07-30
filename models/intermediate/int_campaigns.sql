18f6126fdf66b233edc3a33fffede7dab031217e
doaahussien211@gmail.com <doaahussien211@gmail.com>
2025-07-30 15:10:34 +0000

    union all the stages

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,7 +1,55 @@
-select * from {{ ref('stg_raw_adwords') }}
+with adwords as (
+    select
+        date_date,
+        paid_source,
+        campaign_key,
+        campaign_name,
+        ads_cost,
+        impression,
+        click
+    from {{ ref('stg_raw_adwords') }}
+),
+
+bing as (
+    select
+        date_date,
+        paid_source,
+        campaign_key,
+        campaign_name,
+        ads_cost,
+        impression,
+        click
+    from {{ ref('stg_raw_bing') }}
+),
+
+criteo as (
+    select
+        date_date,
+        paid_source,
+        campaign_key,
+        campaign_name,
+        ads_cost,
+        impression,
+        click
+    from {{ ref('stg_raw_criteo') }}
+),
+
+facebook as (
+    select
+        date_date,
+        paid_source,
+        campaign_key,
+        campaign_name,
+        ads_cost,
+        impression,
+        click
+    from {{ ref('stg_raw_facebook') }}
+)
+
+select * from adwords
 union all
-select * from {{ ref('stg_raw_bing') }}
+select * from bing
 union all
-select * from {{ ref('stg_raw_criteo') }}
+select * from criteo
 union all
-select * from {{ ref('stg_raw_facebook') }}
+select * from facebook
